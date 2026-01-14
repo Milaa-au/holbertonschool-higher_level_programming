1dba66db53e563f56ebf6957e501f2ed521a306c
Milaa-au <mila.audu@icloud.com>
2026-01-14 13:22:04 +0000

    Ajout de la question 06.

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+number = 0
+number2 = 0
+
+for number in range(0, 10):
+    for number2 in range(number + 1, 10):
+        if number == 8 and number2 == 9:
+            print(f"{number}{number2}")
+        else:
+            print(f"{number}{number2}", end=", ")
