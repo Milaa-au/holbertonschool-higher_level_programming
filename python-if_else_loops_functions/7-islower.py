e57e9598ffa992935c78f19cc55830d587102bfd
Milaa-au <mila.audu@icloud.com>
2026-01-14 13:30:43 +0000

    Ajout de la question 08.

---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def islower(c):
+    if ord('a') <= c <= ord('z'):
+        print("True")
+    else:
+        print("False")
---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for c in s:
+        if 'A' <= c <= 'Z':
+            print(c, end="")
