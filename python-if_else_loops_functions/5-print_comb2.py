74fb52b70e3a6834a20ccdf61be8f5fdd76d58c4
Milaa-au <mila.audu@icloud.com>
2026-01-14 10:25:04 +0000

    Ajout de la question 05.

---
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+number = 0
+
+for i in range (0, 100):
+    if number != 99:
+        print("{:02}".format(number), end=", ")
+    if number == 99:
+        print("{}\n".format(number))
+    number = number + 1
