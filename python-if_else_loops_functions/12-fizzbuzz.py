7579b0bfcfd930ff209f5d6f661dc090ffe49fcd
Milaa-au <mila.audu@icloud.com>
2026-01-16 10:44:25 +0000

    correction fizzbuzz

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for a in range(100):
-        if a % 3 == 0 and a % 5 == 0:
-            print("FizzBuzz")
-        elif a % 5 == 0:
-            print("Buzz")
-        elif a % 3 == 0:
-            print("Fizz")
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz ", end="")
+        elif number % 3 == 0:
+            print("Fizz ", end="")
+        elif number % 5 == 0:
+            print("Buzz ", end="")
         else:
-            print(a)
-        if (a < 100):
-            print(" ")
+            print("{} ".format(number), end="")
