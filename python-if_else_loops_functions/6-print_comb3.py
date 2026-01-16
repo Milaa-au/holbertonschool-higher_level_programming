34176294d72b049aad87c80da50409fe756c4e08
Milaa-au <mila.audu@icloud.com>
2026-01-16 10:42:17 +0000

    Correction

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-
-number = 0
-number2 = 0
-
-for number in range(0, 10):
-    for number2 in range(number + 1, 10):
-        if number == 8 and number2 == 9:
-            print(f"{number}{number2}")
+for digit1 in range(0, 10):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8 and digit2 == 9:
+            print("{}{}".format(digit1, digit2))
         else:
-            print(f"{number}{number2}", end=", ")
+            print("{}{}".format(digit1, digit2), end=", ")
