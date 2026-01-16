792954730038d04e1fbeab6b616bb2f9300e4491
Milaa-au <mila.audu@icloud.com>
2026-01-16 10:42:06 +0000

    Correction

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-
-number = 0
-
-for i in range(0, 100):
-    if number != 99:
-        print("{:02}".format(number), end=", ")
+for number in range(0, 100):
     if number == 99:
-        print("{}\n".format(number))
-    number = number + 1
+        print("{}".format(number))
+    else:
+        print("{:02}".format(number), end=", ")
