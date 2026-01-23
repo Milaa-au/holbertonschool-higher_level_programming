a1b6b575c1651aeb68d2cb3ef1dfb4aa7dad428f
Milaa-au <mila.audu@icloud.com>
2026-01-23 12:31:55 +0000

    Erreur de fichier

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, element):
-    new_list = my_list[:]
-    if idx < 0 or idx >= len(my_list):
-        return new_list
-    new_list[idx] = element
-    return new_list
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    for i in range(list_length):
+        res = 0
+        try:
+            try:
+                res = my_list_1[i] / my_list_2[i]
+            except ZeroDivisionError:
+                print("division by 0")
+            except TypeError:
+                print("wrong type")
+        except IndexError:
+            print("out of range")
+        finally:
+            result.append(res)
+    return result
