fe41828184ee8312c61c0c52efa5e05751f1410d
Milaa-au <mila.audu@icloud.com>
2026-01-23 11:23:25 +0000

    Correction

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence:
-        tuple = (len(sentence), sentence[0])
-        print("Length: {} - First character: {}".format(tuple[0], tuple[1]))
+    length = len(sentence)
+    first = sentence[0] if length > 0 else None
+    return (length, first)
