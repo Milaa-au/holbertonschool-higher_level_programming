0df13a18ff96722204e372ab4e7d81ba7fc9c280
Milaa-au <mila.audu@icloud.com>
2026-01-23 11:20:33 +0000

    Correction de la question 01

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if my_list[idx] < 0:
+    if my_list is None:
         return None
-    elif my_list[idx] > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return None
     else:
         return my_list[idx]
