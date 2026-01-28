a9a4238b93afd7bc02b325b5106cad813725c827
Milaa-au <mila.audu@icloud.com>
2026-01-28 12:59:58 +0000

    Correction des commentaire selon pycodestyle.

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,6 +3,9 @@
 
 
 class Square:
-    """We initialize a private instance attribute named size, which will be the size of our square."""
+    """
+    We initialize a private instance attribute named size,
+    which will be the size of our square.
+    """
     def __init__(self, size):
         self.__size = size
