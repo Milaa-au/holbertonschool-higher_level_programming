53e28c93f8a896bf017c11a1fd8eadec00c2ccce
Milaa-au <mila.audu@icloud.com>
2026-01-29 09:07:13 +0000

    Ajout des commentaires selon pycodestyle

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
-class Square:
+"""It is a class named Square that defines a square."""
 
+
+class Square:
+    """
+    The __init__ method initializes a new instance of the Square class.
+    It takes one optional parameter, size, which represents t
+    he length of the sides of the square and defaults to 0.
+
+    The method checks that size is an
+    integer and raises a TypeError if it is not.
+    It also verifies that size is not
+    negative and raises a TypeError if the value is less than 0.
+    If all checks pass, the value of size is
+    stored as a private instance attribute.
+    """
     def __init__(self, size=0):
 
         if not isinstance(size, int):
@@ -9,5 +23,9 @@ class Square:
             raise TypeError("size must be >= 0")
         self.__size = size
 
+    """
+    The  area is public instance method and
+    that returns the current square area
+    """
     def area(self):
         return self.__size * self.__size
