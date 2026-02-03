7ebbff9d1acad1ef6aa290818e36fc9363b60dfc
Milaa-au <mila.audu@icloud.com>
2026-02-03 13:40:04 +0000

    Ajout de la question 06

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Fonction qui renvoie true ou false en fonction que si 
+l'objet est une instance de la class donné."""
+
+class BaseGeometry:
+    def area(self):
+        raise Exception("area() is not implemented")
