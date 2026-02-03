0ee84c72c6252c1cb802659df6675b698c35f665
Milaa-au <mila.audu@icloud.com>
2026-02-03 14:02:34 +0000

    Ajout de la question 08.

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Fonction qui renvoie true ou false en fonction que si 
+l'objet est une instance de la class donné."""
+
+
+class BaseGeometry:
+    def area(self):
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        elif value <= 0:
+            raise TypeError(f"{name} must be greater than 0")
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.integer_validator("height", height)
+        self.integer_validator("width", width)
+        self.__width = width
+        self.__height = height
