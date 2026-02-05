70406dcccf51fea665a6831c1d8c71ba3115370d
Milaa-au <mila.audu@icloud.com>
2026-02-05 14:36:24 +0000

    SAVE

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Fonction qui renvoie true ou false en fonction que si 
+"""Fonction qui renvoie true ou false en fonction que si
 l'objet est une instance de la class donné."""
+
 
 class BaseGeometry:
     """
