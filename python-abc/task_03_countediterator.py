022f79a716aa27694fe146e44c2c2bd1bd9bd6f1
Milaa-au <mila.audu@icloud.com>
2026-02-05 13:04:13 +0000

    Ajout de la question 03.

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+class CountedIterator:
+    def __init__(self, iterable):
+        self.__iterator = iter(iterable)
+        self.__counter = 0
+
+    def get_count(self):
+        return self.__counter
+
+    def __next__(self):
+        item = next(self.__iterator)
+        self.__counter += 1
+        return item
+
+    def __iter__(self):
+        return self
