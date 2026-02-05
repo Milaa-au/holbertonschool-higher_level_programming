f1ee48c729d00ba404f949c768a7095df013fc5a
Milaa-au <mila.audu@icloud.com>
2026-02-05 13:30:40 +0000

    Correction

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""Class qui hérite de (list) et qui contient une fonction qui 
+"""Class qui hérite de (list) et qui contient une fonction qui
 affiche la liste hérité par ordre croissant sans la modifier"""
+
 
 class MyList(list):
 
-    """Fonction qui affiche la liste self tirée par ordre croissant et sans la modifier"""
+    """Fonction qui affiche la liste self tirée
+    par ordre croissant et sans la modifier"""
     def print_sorted(self):
         print(sorted(self))
