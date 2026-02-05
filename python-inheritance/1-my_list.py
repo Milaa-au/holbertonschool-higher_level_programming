180a00d4da88aaac6fc41f3beaa0dbf8aca20de5
Milaa-au <mila.audu@icloud.com>
2026-02-05 14:46:29 +0000

    Correction

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,8 +4,10 @@ affiche la liste hérité par ordre croissant sans la modifier"""
 
 
 class MyList(list):
-
     """Fonction qui affiche la liste self tirée
     par ordre croissant et sans la modifier"""
+
     def print_sorted(self):
+        """Fonction qui affiche la liste self tirée
+        par ordre croissant et sans la modifier"""
         print(sorted(self))
