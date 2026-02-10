e825c4fbde2c30b6c21a7709e40477388ee0604c
Milaa-au <mila.audu@icloud.com>
2026-02-10 13:04:42 +0000

    Ajout d'une fonction qui renvoie un objet (type structure de donnes python) reprsent par une chaine JSON.

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+fonction qui renvoie un objet (structure
+de données Python) représenté par une chaîne JSON
+"""
+
+
+def from_json_string(my_str):
+    """
+    A Modifier plus tard.
+    """
+    return json.loads(my_str)
