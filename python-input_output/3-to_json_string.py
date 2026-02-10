1e0e3d77eb4c70ba0be06d1476e2ebdd7cf2ac50
Milaa-au <mila.audu@icloud.com>
2026-02-10 12:56:58 +0000

    Ajout d'une fonction qui renvoie la representation json d'un objet.

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Fonction qui écrit une chaîne a la fin d'un fichier
+et qui renvoie le nombre de caractères ajoutés. 
+"""
+
+
+def to_json_string(my_obj) :
+    """
+    A Modifier plus tard.
+    """
+    return str(my_obj)
