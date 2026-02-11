a8f7bd3b7342b99d4bba24042a231478363df225
Milaa-au <mila.audu@icloud.com>
2026-02-11 13:27:59 +0000

    Ajout de la question 07.

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Fonction qui écrit un objet dans un fichier texte,
+en utilisant une représentation JSON.
+"""
+import sys
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
+
+filename = "add_item.json"
+
+try:
+    items = load_from_json_file(filename)
+except FileNotFoundError:
+    items = []
+
+items.extend(sys.argv[1:])
+
+save_to_json_file(items, filename)
