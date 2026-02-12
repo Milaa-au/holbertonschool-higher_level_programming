7d94ce728bb1d94f91b89f9682276bb1a9c3e29a
Milaa-au <mila.audu@icloud.com>
2026-02-12 09:52:35 +0000

    Ajout de la question 08.

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Fonction qui écrit un objet dans un fichier texte,
+en utilisant une représentation JSON.
+"""
+
+def class_to_json(obj):
+    """A modifier"""
+    return obj.__dict__
