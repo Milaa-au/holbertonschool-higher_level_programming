a6817764c395c8d17436d0879fccc246a079d66e
Milaa-au <mila.audu@icloud.com>
2026-02-13 10:46:40 +0000

    Ajout des commentaires pour la question 05 .

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """
-Fonction qui écrit un objet dans un fichier texte,
-en utilisant une représentation JSON.
+Définit une fonction qui écrit un
+objet dans un fichier au format JSON.
 """
-import json 
+import json
 
 
 def save_to_json_file(my_obj, filename):
+    """
+    Écrire un objet Python dans un
+    fichier en utilisant le format JSON.
+    """
     with open("data.json", "w", encoding="utf-8") as f:
         json.dump(my_obj, f)
