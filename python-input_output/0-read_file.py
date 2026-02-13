b42f5970b51b6fb02a7fecec55dd38a523763734
Milaa-au <mila.audu@icloud.com>
2026-02-13 10:42:21 +0000

    Correction pycodestyle

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
-"""Ce module définit une fonction qui lit un fichier texte et affiche son contenu."""
+"""
+Ce module définit une fonction qui lit un
+fichier texte et affiche son contenu.
+"""
 
 
 def read_file(filename=""):
-    """Lit un fichier texte (UTF-8) et affiche son contenu sur la sortie standard."""
+    """
+    Lit un fichier texte (UTF-8) et
+    affiche son contenu sur la sortie standard.
+    """
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
