4c8290af6a211156ad4b02806292097877864612
Milaa-au <mila.audu@icloud.com>
2026-02-13 13:37:24 +0000

    Correction

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -6,8 +6,8 @@ méthode pour obtenir certains attributs."""
 class Student:
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
-        self.age = age
         self.last_name = last_name
+        self.age = age
 
     def to_json(self):
         """Retourner un dictionnaire des
