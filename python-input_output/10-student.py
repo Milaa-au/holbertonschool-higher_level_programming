69d20a2af0d4f7a187e099536244041d9f4a26fc
Milaa-au <mila.audu@icloud.com>
2026-02-13 13:39:16 +0000

    Correction

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,27 +1,23 @@
-#!/usr/bin/python3
-"""Définit une classe Student avec
-méthode pour obtenir certains attributs."""
-
-
 class Student:
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """Retourner un dictionnaire des
-        attributs sélectionnés de l'étudiant."""
+    def to_json(self, attrs=None):
+        """
+        Retourne un dictionnaire
+        des attributs sélectionnés de l'étudiant.
+        
+        Args:
+            attrs (list, optional): liste
+            d'attributs à inclure. Si None,
+            retourne tous les attributs.
+        """
         obj = self.__dict__.copy()
-        if type(attrs) is list:
-            for item in attrs:
-                if type(item) is not str:
-                    return obj
-            d_list = {}
-            for iatr in range(len(attrs)):
-                for satr in obj:
-                    if attrs[iatr] == satr:
-                        d_list[satr] = obj[satr]
-            return d_list
-        return obj
+        if attrs is None:
+            return obj
 
+        if type(attrs) is list and all(isinstance(a, str) for a in attrs):
+            return {key: obj[key] for key in attrs if key in obj}
+        return obj
