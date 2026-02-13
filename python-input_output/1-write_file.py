d2e3f935496b5d268f43b937c78007a6125c6167
Milaa-au <mila.audu@icloud.com>
2026-02-13 10:43:01 +0000

    Correction pycodestyle pour le fichier 1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
-"""Définit une fonction qui écrit une chaîne dans un fichier texte UTF-8."""
+"""
+Définit une fonction qui écrit
+une chaîne dans un fichier texte UTF-8.
+"""
 
 
 def write_file(filename="", text=""):
-    """Écrire une chaîne dans un fichier et retourner le nombre de caractères écrits."""
+    """
+    Écrire une chaîne dans un fichier
+    et retourner le nombre de caractères écrits.
+    """
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
