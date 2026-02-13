0cb521dbfb83d06f680d1d2d32b665f3f63915b8
Milaa-au <mila.audu@icloud.com>
2026-02-13 13:41:46 +0000

    Correction

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,4 +1,14 @@
+#!/usr/bin/python3
+"""
+Définit une classe Student avec
+méthodes pour gérer ses attributs en JSON.
+"""
+
 class Student:
+    """
+    Définit une classe Student avec
+    méthodes pour gérer ses attributs en JSON.
+    """
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
