c4ad2b2661f5066e632adb2a5a2c672cfdd54f4d
Milaa-au <mila.audu@icloud.com>
2026-02-19 14:56:54 +0000

    Correction

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -21,10 +21,10 @@ class Simpleapi(http.server.BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
         elif self.path == '/data':
-            dataset = {"name": "John", "age": 30, "city": "New York"}
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
+            dataset = {"name": "John", "age": 30, "city": "New York"}
             self.wfile.write(json.dumps(dataset).encode("utf-8"))
         elif self.path == '/status':
             self.send_response(200)
