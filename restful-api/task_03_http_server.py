08bac622bf653eb42e5174dc2f7672f8e8874c8d
Milaa-au <mila.audu@icloud.com>
2026-02-19 12:58:26 +0000

    Ajout du script sans les descriptions mais avec pycodestyle.

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+"""
+"""
+import json
+import http.server
+import socketserver
+
+
+class Simpleapi(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+        elif self.path == '/data':
+            dataset = {"name": "John", "age": 30, "city": "New York"}
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps(dataset).encode("utf-8"))
+        elif self.path == '/status':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"OK")
+        elif self.path == '/info':
+            datainfo = {"version": "1.0", "description":
+                        "A simple API built with http.server"}
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(json.dumps(datainfo).encode("utf-8"))
+        else:
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"Endpoint not found")
+
+
+PORT = 8000
+
+with socketserver.TCPServer(("", PORT), Simpleapi) as httpd:
+    httpd.serve_forever()
