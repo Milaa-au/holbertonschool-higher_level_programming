40a8c76e09085e6cc64fead4e16dcb4ad5d7acb3
Milaa-au <mila.audu@icloud.com>
2026-03-05 12:54:16 +0000

    Correction

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- script that creates the MySQL server user user_0d_1.
+-- Coucou
 CREATE USER IF NOT EXIST 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 FLUSH PRIVILEGES;
