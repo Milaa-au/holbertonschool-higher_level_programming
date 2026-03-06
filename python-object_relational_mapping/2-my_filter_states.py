90962d3f38e69ef5954c7e3e162c0541b08520da
Milaa-au <mila.audu@icloud.com>
2026-03-06 13:13:38 +0000

    Correction

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -5,6 +5,7 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
+
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
@@ -13,10 +14,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        charset="utf8",
         user=username,
         passwd=password,
-        db=database,
+        db=database
     )
 
     cur = db.cursor()
