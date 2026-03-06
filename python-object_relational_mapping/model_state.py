21cd61908285dbc458da3d2fc44c18c96410e9eb
Milaa-au <mila.audu@icloud.com>
2026-03-06 13:58:34 +0000

    Correction

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -3,11 +3,11 @@
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
+mymetadata = MetaData()
 Base = declarative_base(metadata=mymetadata)
 
 class State(Base):
     """desc"""
-    __tablenam__ = 'state'
-
+    __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
