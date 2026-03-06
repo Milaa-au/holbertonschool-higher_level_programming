1b3b68618df3142073dfaa91293bed5f022caf35
Milaa-au <mila.audu@icloud.com>
2026-03-06 10:44:13 +0000

    Ajout de la question 01.

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """Connect to the DB using command line arguments."""
+    db = MySQLdb.connect(
+         host="localhost",
+         port=3306,
+         user=sys.argv[1],
+         pwd=sys.argv[2],
+         db=sys.argv[3]
+    )
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
+
+    cur.close()
