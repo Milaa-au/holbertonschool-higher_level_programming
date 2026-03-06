4d45aa5c005e5ee40a8ac3a9b8017b0d128159d4
Milaa-au <mila.audu@icloud.com>
2026-03-06 14:33:07 +0000

    correction

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -3,5 +3,8 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
     id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    state INT 
-)
+    state INT NOT NULL FOREIGN KEY,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY(id)
+    FOREIGN KEY(state_id) REFERENCES states(id)
+);
