c348cb18d66a78abaefde01c54a658327fe1c951
Milaa-au <mila.audu@icloud.com>
2026-03-06 12:53:03 +0000

    correction

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
+"""A ajouter."""
+
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3])
+        db=sys.argv[3]
+    )
 
     cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
 
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
-    rows = cur.fetchall()
-    for row in rows:
+    for row in cur.fetchall():
         print(row)
 
     cur.close()
