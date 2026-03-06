372a8029f717690f3e892ed38a4a587c0d342fa7
Milaa-au <mila.audu@icloud.com>
2026-03-06 10:45:43 +0000

    Remise comme c'etat avant car le bug ne venait pas de mons cript

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,22 +6,29 @@ import MySQLdb
 import sys
 
 
-if __name__ == "__main__":
-    """fonction that lists all states from the database hbtn_0e_0_usa"""
+def select_states():
+    """Takes arguments argv to list from database
 
+    Arguments:
+        argv[1]: mysql username
+        argv[2]: mysql password
+        argv[3]: database name
+    """
     db = MySQLdb.connect(host="localhost",
                          port=3306,
                          user=sys.argv[1],
-                         pwd=sys.argv[2],
+                         passwd=sys.argv[2],
                          db=sys.argv[3])
 
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
-
-    states = cur.fetchall()
-    for state in states:
-        print(state)
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
 
     cur.close()
     db.close()
+
+if __name__ == "__main__":
+    select_states()
